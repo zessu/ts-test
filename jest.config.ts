415cd3dc36e810423b248ab13fc6f388b954c008
Loopcruitment <nobody@nowhere.net>
2021-07-23 20:36:33 +1000

    shift jest config to typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(src|test)/.*(\\.|-|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'json', 'js', 'jsx'],
 };
+
+export default config;
